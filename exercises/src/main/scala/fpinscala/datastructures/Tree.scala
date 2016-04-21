fafc32e18ac984157e3a7cf500646aade664785b
Szilard Kovacs <kovszilard@gmail.com>
2016-04-21 15:19:26 +0200

    exercise 3.29

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -28,6 +28,9 @@ object Tree {
     case Branch(l, r) => Branch(map(l)(f), map(r)(f))
   }
 
-  def fold[A,B](t: Tree[A])(l: A => B)(b: (B,B) => B): B = ???
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = t match {
+    case Leaf(v) => f(v)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
 
 }
